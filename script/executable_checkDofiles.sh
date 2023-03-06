25cfb49398017d8881787e18cc68b591edf46131
promecarus <haikalslipi@gmail.com>
2023-03-06 09:29:16 +0700

    Create executable_checkDofiles.sh

---
+++ script/executable_checkDofiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chezmoi add ~/.bashrc
+chezmoi add ~/.oh-my-zsh/custom/aliases.zsh
+chezmoi add ~/.oh-my-zsh/themes/promecarus.zsh-theme
+chezmoi add ~/.zshrc
+chezmoi add ~/script/checkDofiles.sh
