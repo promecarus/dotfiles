7a18a0788870aa1b51cb4c3b1d66a1b16e3becaa
promecarus <haikalslipi@gmail.com>
2023-03-10 07:58:52 +0700

    Update executable_checkDofiles.sh

--- script/executable_checkDofiles.sh
+++ script/executable_checkDofiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 chezmoi add ~/.bashrc
+chezmoi add ~/.jupyter/jupyter_notebook_config.py
 chezmoi add ~/.oh-my-zsh/custom/aliases.zsh
 chezmoi add ~/.oh-my-zsh/themes/promecarus.zsh-theme
 chezmoi add ~/.zshrc
